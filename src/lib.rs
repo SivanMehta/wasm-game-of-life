5e09ba260b3e78b4ce22a892744472cc8fde6394
Sivan Mehta <sivanmehta@microsoft.com>
2022-06-08 08:12:54 -0700

    reinit

---
+++ src/lib.rs
@@ -0,0 +1,138 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32, // 32 bits
+    height: u32, // 32 bits
+    cells: Vec<Cell> // a vector of cells
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn width(&self) -> u32 {
+        return self.width;
+    }
+
+    pub fn height(&self) -> u32 {
+        return self.height;
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        return self.cells.as_ptr();
+    }
+
+    // translate a given row and column into an index
+    fn get_index(&self, row: u32, col: u32) -> usize {
+        return (row * self.width + col) as usize
+    }
+
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0 ..self.width  {
+                let index = self.get_index(row, col);
+                let cell = self.cells[index];
+                let neighbors = self.live_neighbor_count(row, col);
+                let next_cell = match(cell, neighbors) {
+                    // any live cell with fewer than 2 neighbors dies (underpopulation)
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+        
+                    // any live cell with two or three live neighbors lives on
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+        
+                    // any live cell with more than three neighbors dies (overpopulation)
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+        
+                    // any blank cell with 3 live neighbors lives (creation of life)
+                    (Cell::Dead, 3) => Cell::Alive,
+        
+                    // otherwise
+                    (otherwise, _) => otherwise,
+                };
+        
+                next[index] = next_cell;
+            }
+        }
+
+        self.cells = next;
+    }
+
+    pub fn new(size: u32) -> Universe {
+        let width = size;
+        let height = size;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 7 == 0 || i % 2 == 0 {
+                    return Cell::Alive
+                } else {
+                    return Cell::Dead
+                }
+            })
+            .collect();
+        
+        let creation = Universe {
+            width,
+            height,
+            cells,
+        };
+
+        return creation;
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+}
+
+use std::fmt;
+
+// affords a to_string method
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // as_slice.chunks will essentially allow us to write this as a nessted loop
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
---
+++ www/index.js
@@ -0,0 +1,60 @@
+import { Universe, Cell } from "hello-wasm-pack";
+// Import the WebAssembly memory at the top of the file.
+import { memory } from "hello-wasm-pack/hello_wasm_pack_bg";
+
+const SIZE = 64;
+const CELL_SIZE = 10; // px
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+// Construct the universe, and get its width and height.
+const universe = Universe.new(SIZE);
+const width = universe.width();
+const height = universe.height();
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById("canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+function getIndex(row, column) {
+    return row * width + column;
+}
+
+function drawCells() {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+        const idx = getIndex(row, col);
+
+        ctx.fillStyle = cells[idx] === Cell.Dead
+            ? DEAD_COLOR
+            : ALIVE_COLOR;
+
+        ctx.fillRect(
+            col * (CELL_SIZE + 1) + 1,
+            row * (CELL_SIZE + 1) + 1,
+            CELL_SIZE,
+            CELL_SIZE
+        );
+        }
+    }
+
+    ctx.stroke();
+};
+
+function renderLoop() {
+    drawCells();
+    universe.tick();
+    requestAnimationFrame(renderLoop);
+  };
+  
+
+requestAnimationFrame(renderLoop);
