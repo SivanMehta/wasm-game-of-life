7510299ebde20b6e71b7f8435a1211c6ad648e38
Sivan Mehta <sivanmehta@microsoft.com>
2022-06-09 17:03:00 -0700

    squeeze 9 extra fps

--- www/index.js
+++ www/index.js
@@ -5,8 +5,8 @@ import { memory } from "hello-wasm-pack/hello_wasm_pack_bg";
 import FPS from './fps';
 const fps = new FPS();
 
-const SIZE = 96;
-const CELL_SIZE = 8; // px
+const SIZE = 150;
+const CELL_SIZE = 4; // px
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 const GRID_COLOR = '#CCCCCC';
@@ -58,20 +58,34 @@ function initCells() {
     const cellsPtr = universe.cells();
     const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
 
+    // alive cells
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
-        const idx = getIndex(row, col);
+            const idx = getIndex(row, col);
+            if(!bitIsSet(idx, cells)) continue;
+            ctx.fillRect(
+                col * (CELL_SIZE) + 1,
+                row * (CELL_SIZE) + 1,
+                CELL_SIZE - 1,
+                CELL_SIZE - 1
+            );
 
-        ctx.fillStyle = bitIsSet(idx, cells)
-            ? ALIVE_COLOR
-            : DEAD_COLOR;
+        }
+    }
 
-        ctx.fillRect(
-            col * (CELL_SIZE) + 1,
-            row * (CELL_SIZE) + 1,
-            CELL_SIZE - 1,
-            CELL_SIZE - 1
-        );
+    // dead cells
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if(bitIsSet(idx, cells)) continue;
+            ctx.fillRect(
+                col * (CELL_SIZE) + 1,
+                row * (CELL_SIZE) + 1,
+                CELL_SIZE - 1,
+                CELL_SIZE - 1
+            );
         }
     }
 };
